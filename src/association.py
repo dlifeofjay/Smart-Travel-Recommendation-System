75dad1f2256703d65bb0da532f5d7a921ad5f550
dlifeofjay <jubrilifekoya@gmail.com>
2026-06-06 13:16:00 +0100

    deployment and update

--- src/association.py
+++ src/association.py
@@ -46,5 +46,8 @@ def associate(df):
     print("Association rule made")
 
     # Needed columns for recommendation engine
+    rl[["antecedents", "consequents", "confidence"]].to_csv(r"C:\Users\USER\Documents\MY PORTFOLIO\Smart Travel Recommendation System\Travel Agency\Recommendation data.csv", index=False)
+
     return rl[["antecedents", "consequents", "confidence"]]
+
 
